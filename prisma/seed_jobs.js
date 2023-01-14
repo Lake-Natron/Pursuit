8483b089d5ddc08f5daa65fc36a572870043a56b
Callie Stoscup <calliestoscup@gmail.com>
2023-01-14 17:06:34 -0500

    Finished job search route with all filters but location

--- prisma/seed_jobs.js
+++ prisma/seed_jobs.js
@@ -9,7 +9,10 @@ let jobs = [
     description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
     salary: 80000,
     location: '27540',
-    close_date: new Date('2023-01-31')
+    close_date: new Date('2023-01-31'),
+    experience_type: 'Entry Level',
+    employment_type: 'Full-Time',
+    jobsite: 'Remote'
   },
   {
     name: 'Software Engineer',
@@ -17,7 +20,10 @@ let jobs = [
     description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
     salary: 100000,
     location: '37421',
-    close_date: new Date('2023-01-31')
+    close_date: new Date('2023-01-31'),
+    experience_type: 'Mid Level',
+    employment_type: 'Full-Time',
+    jobsite: 'Onsite'
   },
   {
     name: 'HR Generalist',
@@ -25,7 +31,10 @@ let jobs = [
     description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
     salary: 65000,
     location: '11791',
-    close_date: new Date('2023-01-31')
+    close_date: new Date('2023-01-31'),
+    experience_type: 'Senior Level',
+    employment_type: 'Part-Time',
+    jobsite: 'Hybrid'
   },
   {
     name: 'Corporate Trainer',
@@ -33,7 +42,10 @@ let jobs = [
     description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
     salary: 70000,
     location: '43512',
-    close_date: new Date('2023-01-31')
+    close_date: new Date('2023-01-31'),
+    experience_type: 'Executive Level',
+    employment_type: 'Contract',
+    jobsite: 'Remote'
   },
   {
     name: 'Front End Developer',
@@ -41,7 +53,10 @@ let jobs = [
     description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
     salary: 90000,
     location: '11111',
-    close_date: new Date('2023-01-31')
+    close_date: new Date('2023-01-31'),
+    experience_type: 'Entry Level',
+    employment_type: 'Internship',
+    jobsite: 'Remote'
   }
 ];
 
--- server/index.js
+++ server/index.js
@@ -5,9 +5,34 @@ let app = express();
 const { PrismaClient } = require('@prisma/client');
 const prisma = new PrismaClient();
 
+app.get('/job', async (req, res) => {
+  const { id } = req.query;
+  const job = await prisma.Job.findUnique({
+    where: { id: Number(id) }
+  });
+  res.send(job);
+})
+
+app.get('/jobs/search', async (req, res) => {
+  const { company_id, experience_type, employment_type, jobsite, salary_min, salary_max } = req.query;
+  const jobs = await prisma.Job.findMany({
+    where: {
+      company_id: Number(company_id),
+      experience_type: experience_type,
+      employment_type: employment_type,
+      jobsite: jobsite,
+      salary: {
+        gte: Number(salary_min) || 0,
+        lte: Number(salary_max) || 1000000000
+      }
+    }
+  });
+  res.send(jobs);
+})
+
 app.get('/jobs', async (req, res) => {
   const jobs = await prisma.Job.findMany();
-  res.json(jobs);
+  res.send(jobs);
 })
 
 app.listen(process.env.PORT, () => console.log('Listening on port ' + process.env.PORT));

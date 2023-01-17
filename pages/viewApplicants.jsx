5bdf9bbd6f4a13a7137d0defcc082f9aad51d6de
carterbrooks <carter.brooks91@gmail.com>
2023-01-17 14:08:32 -0600

    view applicants initial setup

---
+++ pages/viewApplicants.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import NavBar from '../src/navBar';
+import ApplicantListCard from '../src/applicantListCard';
+
+const ViewApplicants = () => {
+  return (
+    <>
+      <NavBar />
+    </>
+  )
+}
+
+export default ViewApplicants;
---
+++ src/applicantListCard.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+const ApplicantListCard = () => {
+  return (
+    <>
+    </>
+  )
+}
+
+export default ApplicantListCard;

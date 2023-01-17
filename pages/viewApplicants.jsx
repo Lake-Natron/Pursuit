1f3f520d9ecf7a35851de5af357c95c55c332897
carterbrooks <carter.brooks91@gmail.com>
2023-01-17 14:25:32 -0600

    implement list, add not interested/interested buttons

--- pages/viewApplicants.jsx
+++ pages/viewApplicants.jsx
@@ -1,11 +1,35 @@
 import React from "react";
+import { useState, useEffect } from 'react';
 import NavBar from '../src/navBar';
 import ApplicantListCard from '../src/applicantListCard';
+import Box from '@mui/material/Box';
+import List from '@mui/material/List';
 
 const ViewApplicants = () => {
+  const [jobListings, setJobListings] = useState([]);
+
+  useEffect(() => {
+    // axios.get('/')
+    // .then(())
+    // .catch(())
+  }, [])
+
   return (
     <>
-      <NavBar />
+    <NavBar />
+    <Box sx={{width: '100%', minWidth: 480, display: 'flex', alignItems: 'center', justifyContent: 'center', border: '1px solid'}}>
+      <nav aria-label="job-list-container">
+        <h2>Have Job Details Here?</h2>
+        <List sx={{mt: -1}}>
+          {jobListings.map((listing, index) =>
+            <ApplicantListCard listing={listing} key={index} />
+          )}
+          <ApplicantListCard />
+          <ApplicantListCard />
+          <ApplicantListCard />
+        </List>
+      </nav>
+    </Box>
     </>
   )
 }
--- src/applicantListCard.jsx
+++ src/applicantListCard.jsx
@@ -1,8 +1,27 @@
-import React from "react";
+import React from 'react';
+import { useState, useEffect } from 'react';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemText from '@mui/material/ListItemText';
+import Button from '@mui/material/Button';
 
 const ApplicantListCard = () => {
+
+  const handleInterestedClick = (e) => {
+    e.preventDefault()
+  }
+
+  const handleNotInterestedClick = (e) => {
+    e.preventDefault()
+  }
+
   return (
     <>
+    <ListItem sx={{border:'1px solid grey', width:'60vw', minHeight:'7em', marginBottom: '1em', borderRadius: '8px', overflow: 'hidden'}}>
+        <ListItemText primary='Applicant Details' />
+        <Button variant="contained" onClick={handleInterestedClick}>Interested</Button>
+        <Button variant="contained" onClick={handleNotInterestedClick}>Not Interested</Button>
+    </ListItem>
     </>
   )
 }

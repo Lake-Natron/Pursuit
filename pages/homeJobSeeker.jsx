774979bfd993b7078ea67bc0f81c10b00a39db91
carterbrooks <carter.brooks91@gmail.com>
2023-01-17 12:36:01 -0600

    break up job listing sections

--- pages/homeJobSeeker.jsx
+++ pages/homeJobSeeker.jsx
@@ -5,7 +5,7 @@ import Box from '@mui/material/Box';
 import Container from '@mui/material/Box';
 import List from '@mui/material/List';
 import ListItem from '@mui/material/ListItem';
-import JobSeekerJobListCard from '../src/JobSeekerJobListCard'
+import JobSeekerJobListCard from '../src/JobSeekerJobListCard';
 
 const HomeJobSeeker = () => {
   const [jobListings, setJobListings] = useState([]);
@@ -23,14 +23,23 @@ const HomeJobSeeker = () => {
     </div>
     <Box sx={{width: '100%', minWidth: 480, display: 'flex', alignItems: 'center', justifyContent: 'center', border: '1px solid'}}>
       <nav aria-label="job-list-container">
-        <List sx={{mt: 4}}>
+        <h2>Extremely Interested</h2>
+        <List sx={{mt: -1}}>
           {jobListings.map((listing, index) =>
             <JobSeekerJobListCard listing={listing} key={index} />
           )}
           <JobSeekerJobListCard />
           <JobSeekerJobListCard />
           <JobSeekerJobListCard />
+        </List>
+        <h2>Very Interested</h2>
+        <List sx={{mt: -1}}>
           <JobSeekerJobListCard />
+          <JobSeekerJobListCard />
+          <JobSeekerJobListCard />
+        </List>
+        <h2>Interested</h2>
+        <List sx={{mt: -1}}>
           <JobSeekerJobListCard />
           <JobSeekerJobListCard />
           <JobSeekerJobListCard />

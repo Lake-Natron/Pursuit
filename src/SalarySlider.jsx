8843932dce03a5ab221f6dfff842fa8f3184787f
Nika Woodfill <nikawoodfill@Nikas-MacBook-Air-2.local>
2023-01-20 14:28:05 -0500

    filter search bar

---
+++ src/SalarySlider.jsx
@@ -0,0 +1,94 @@
+import * as React from 'react';
+import PropTypes from 'prop-types';
+import Slider, { SliderThumb } from '@mui/material/Slider';
+import { styled } from '@mui/material/styles';
+import Typography from '@mui/material/Typography';
+import Tooltip from '@mui/material/Tooltip';
+import Box from '@mui/material/Box';
+
+function SalarySlider() {
+
+const iOSBoxShadow =
+  '0 3px 1px rgba(0,0,0,0.1),0 4px 8px rgba(0,0,0,0.13),0 0 0 1px rgba(0,0,0,0.02)';
+
+const marks = [
+  {
+    value: 0,
+    label: '$0',
+  },
+  {
+    value: 100,
+    label: '$50k'
+  },
+  {
+    value: 250,
+    label: '$250k',
+  },
+  {
+    value: 500,
+    label: '$500k',
+  },
+];
+
+const IOSSlider = styled(Slider)(({ theme }) => ({
+  color: theme.palette.mode === 'dark' ? '#E44F48' : '#E44F48',
+  height: 2,
+  padding: '15px 0',
+  '& .MuiSlider-thumb': {
+    height: 28,
+    width: 28,
+    backgroundColor: '#fff',
+    boxShadow: iOSBoxShadow,
+    '&:focus, &:hover, &.Mui-active': {
+      boxShadow:
+        '0 3px 1px rgba(0,0,0,0.1),0 4px 8px rgba(0,0,0,0.3),0 0 0 1px rgba(0,0,0,0.02)',
+      // Reset on touch devices, it doesn't add specificity
+      '@media (hover: none)': {
+        boxShadow: iOSBoxShadow,
+      },
+    },
+  },
+  '& .MuiSlider-valueLabel': {
+    fontSize: 12,
+    fontWeight: 'normal',
+    top: -6,
+    backgroundColor: 'unset',
+    color: theme.palette.text.primary,
+    '&:before': {
+      display: 'none',
+    },
+    '& *': {
+      background: 'transparent',
+      color: theme.palette.mode === 'dark' ? '#fff' : '#000',
+    },
+  },
+  '& .MuiSlider-track': {
+    border: 'none',
+  },
+  '& .MuiSlider-rail': {
+    opacity: 0.5,
+    backgroundColor: '#bfbfbf',
+  },
+  '& .MuiSlider-mark': {
+    backgroundColor: '#bfbfbf',
+    height: 8,
+    width: 1,
+    '&.MuiSlider-markActive': {
+      opacity: 1,
+      backgroundColor: 'currentColor',
+    },
+  },
+}));
+    return (
+      <Box sx={{ml:"10px", mt: "30px", width: 320, display: "inline-block" }}>
+        <IOSSlider 
+          //value={value}
+          aria-label="ios slider"
+          marks={marks}
+          min={0}
+          max={500}
+          defaultValue={[90, 200]}
+          valueLabelDisplay="on"
+        /></Box>)
+}
+export default SalarySlider;
